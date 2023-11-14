b6a5d329e1a96ecaa799796659da8e5219521e4b
Stanisław <stasiudoeringw@gmail.com>
2023-11-14 15:07:15 +0100

    gdg

---
+++ zajeciea01/zajecia.py
@@ -0,0 +1,18 @@
+# x=0
+# while float(x)<=1:
+#     x+=0.1
+#     print(x)
+
+# delta
+
+import math
+a=int(1)
+b=int2
+c=3
+delta=(b*b)-4*a*c
+x1=(-b+math.sqrt(delta))//2*a
+x2=(-b-math.sqrt(delta)//2*a
+
+
+print(a)
+print(b)
