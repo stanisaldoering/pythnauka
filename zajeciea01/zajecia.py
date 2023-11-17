830e34db68dad1afb903387fe60ebf27ba81aed0
Stanisław <stasiudoeringw@gmail.com>
2023-11-17 10:23:58 +0100

    błedy

--- zajeciea01/zajecia.py
+++ zajeciea01/zajecia.py
@@ -5,14 +5,14 @@
 
 # delta
 
-import math
-a=int(1)
-b=int2
-c=3
-delta=(b*b)-4*a*c
-x1=(-b+math.sqrt(delta))//2*a
-x2=(-b-math.sqrt(delta)//2*a
-
-
-print(a)
-print(b)
+# import math
+# a=int(1)
+# b=int(2)
+# c=3
+# delta=(b*b)-4*a*c
+# x1=(-b+math.sqrt(delta))//2*a
+# x2=(-b-math.sqrt(delta)//2*a
+#
+#
+# print(a)
+# print(b)
