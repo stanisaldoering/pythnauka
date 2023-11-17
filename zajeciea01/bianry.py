e688f0ae6232aa7eb9cbb64156b281bb5d3ef3be
Stanisław <stasiudoeringw@gmail.com>
2023-11-17 10:29:36 +0100

    błedy

--- zajeciea01/bianry.py
+++ zajeciea01/bianry.py
@@ -7,5 +7,12 @@ def binFloat(n,ilebit=0):
         else:
             return 'error'
     return(wynik)
-print(binFloat("0,1111",4))
 
+
+print("y1",x-y1)
+print("y2",x-y2)
+print("y3",x-y3)
+# wratosc dokladna x=0,1 w systemie dziesietny jest rowna 0,1 oblicz jaki bedzie blad przy zapisie liczby 0,1 z wykorzytsaniem 8 bit,12, 16
+
+# napisz program  ktroy obliczby  wartosc sinusa 1 korzystajac ze wzoru sin
+# obliczenia wykonaj z dokladnoscia do 4 liczb
