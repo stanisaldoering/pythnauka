9b488052746acfeafb209fad136a2baf51ff0bad
stanisaldoering <149144184+stanisaldoering@users.noreply.github.com>
2023-11-22 18:04:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# zadanie 1 
+
+
+def silnia(n):
+  w=1
+  for i in range(1,n+1):
+    w*=1
+  return w
+
+def cos():
+  eps=0.001
+  result=1
+  k=2
+  sklad=1/silnia(2)
+  znak=+1
+  while sklad>=eps:
+    result+=znak*sklad
+    k+=2
+    sklad=1/silnia(k)
+    znak=+znak
+  return result
+print(cos())
+
