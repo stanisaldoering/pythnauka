34c7a6d73ebc42378a7b9e9e3bea6a170e4aca20
Stanisław <stasiudoeringw@gmail.com>
2023-12-12 15:06:36 +0100

    poczatek geo analitycznej

---
+++ zajecia02/zajecia-12-12-2023.py
@@ -0,0 +1,15 @@
+# tekst='hhhajjthaakkacdhkh'
+# wzorzec="abcd"
+# def wzor(tekst,wzorzec):
+#     for i in range(0,len(tekst)-len(wzorzec)-1):
+#         if wzorzec[0]==tekst[i]:
+#             tmp=len(wzorzec)+i
+#             if wzorzec == tekst[i:tmp]:
+#                 return True
+#     return False
+# print(wzor(tekst,wzorzec))
+
+l=[(1,2),(3,4)]
+print(l[0[1]])
+# def dlugosc(l):
+    #
