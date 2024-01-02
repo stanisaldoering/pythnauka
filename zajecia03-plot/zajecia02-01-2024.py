b243dd9943117f1e56ebfd7e8166f73a16476352
Stanisław <stasiudoeringw@gmail.com>
2024-01-02 15:07:09 +0100

    obliczanie pola trojkata do porawy

---
+++ zajecia03-plot/zajecia02-01-2024.py
@@ -0,0 +1,26 @@
+# import matplotlib.pyplot as plt
+# x=[i for i in range(-10,10)]
+# y=[]
+# a=float(input("podaj a"))
+# b=float(input("podaj b"))
+# c=float(input("podaj c"))
+# for i in x:
+#     y.append(a*i**2+b*i+c)
+#
+# plt.plot(x,y)
+# plt.plot(x,y,"ro")
+# # daje kropki
+# plt.plot(color="green",linestyle="--",linewidth=1)
+# plt.show()
+#
+# # napisz program obliczajcy pole trojakta ktorego wierzcholkami sa nie wspolniowe punkty
+# # a(xa,yb)
+# # b(xb,yb)
+# # c(xc,ya)
+# P=1/2(xb-xa)(yc-ya)-(yb-ya)(xc-xa)
+import formatter
+from math import fabs
+a=(1,2)
+b=(3,4)
+c=(5,6)
+return 0.5*fabs(pkt[1][1]-pkt[0][0]*)
